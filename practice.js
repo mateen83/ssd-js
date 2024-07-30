b411fa7908552c23cd29f1c2d7441d6267eb8d00
mateen <mateen8325@gmail.com>
2024-07-30 06:29:52 +0500

    oo

---
+++ practice.js
@@ -0,0 +1,25 @@
+
+
+
+        // const auto = [2,4,5];
+        // let total = 0;
+
+        // for(let x = 0; x < auto.length ; x++){
+        //    total = auto[x];
+        //     total += auto;
+        //     console.log(total);
+
+        // }
+
+
+// console.log('hello');
+
+
+
+// const auto = ['name', 'akel', 'for'];
+
+// // for (let x = 0; x < auto.length; x++) {
+// //       // Print the running total
+// // }
+// console.log(auto);
+ 
