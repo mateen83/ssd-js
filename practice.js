2b1f1d5f4bb1a669de841391823de9a0ed5ec251
mateen <mateen8325@gmail.com>
2024-08-05 02:45:44 +0500

    gg

--- practice.js
+++ practice.js
@@ -23,3 +23,70 @@
 // // }
 // console.log(auto);
  
+
+
+// looping through an array
+
+
+// let arr = ['aro',' axla','dora' ];
+// for (let ind = 0; ind < arr.length ; ind++){
+//         // console.log(ind);
+//         const tky  = arr[ind]
+//         console.log(tky.trim())       
+// }
+
+
+// acumolater pattren to summ all in side the array 
+
+
+// let arr = [4,5,6,7,2];
+// let sum = 0;
+
+
+// for(let x = 0 ; x < arr.length ; x++){
+//      let  kota = arr[x];
+//      sum += kota;
+
+
+// }
+// console.log(sum);
+
+
+
+
+// getLastValue([1, 20, 22, 24, 5]) => 5
+// getLastValue(['hi', 'hello', 'good']) => 'good'
+// 11c. Create a function arraySwap(array) that takes an array and returns an array where the first and last values are swapped (or switched).
+// arraySwap([1, 20, 22, 24, 5]) => [5, 20, 22, 24, 1]
+// arraySwap(['hi', 'hello', 'good']) => ['good', 'hello', 'hi']
+
+
+
+
+
+
+// 11a. Create an array of numbers: const nums = [10, 20, 30]; Modify the last value in this array and change it to 99.
+
+
+let arr = [33,65,78];
+arr[arr.length - 1] = 55;
+
+console.log(arr);
+
+
+
+// 11b. Create a function getLastValue(array) that takes an array and returns the last value in the array. Hint: array indexes start at 0 so the last index is the number of values in the array minus 1 (lastIndex = array.length - 1).
+
+
+
+// function getLastValue(array){
+
+//         let array =[3,5,76,8,3,4];
+//         return array[array.length -1];
+        
+ 
+    
+
+// }
+// console.log(getLastValue(array));
+
